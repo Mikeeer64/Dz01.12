96f9fda930e428e3f07f108e794c34c0f5186f54
Mikeeer <my email>
2022-12-01 16:50:37 +0300

    Example03

---
+++ Example03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите цифру: ");
+int namber = int.Parse(Console.ReadLine());
+
+if (namber == 6 || namber == 7)
+    Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
