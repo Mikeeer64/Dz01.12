91b234ec7ac2b8d5a82818d6adf41381a7e1565d
Mikeeer <my email>
2022-12-01 16:41:58 +0300

    Example02

---
+++ Example02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int namber = int.Parse(Console.ReadLine());
+int answer = 0;
+if (namber >= 100)
+{
+while (namber > 999)
+    {
+        namber = namber / 10; 
+    }
+answer = namber % 10;
+Console.WriteLine($"Ответ: {answer}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
