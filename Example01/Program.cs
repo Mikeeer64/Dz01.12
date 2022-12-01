7c5cd921fed5aa80f6cf5a7a9aabadc08fa6d74d
Mikeeer <my email>
2022-12-01 16:12:46 +0300

    Example01

---
+++ Example01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int namber = int.Parse(Console.ReadLine());
+
+int X1 = namber % 100;
+int answer = X1 / 10;
+
+Console.WriteLine($"Ответ: {answer}");
